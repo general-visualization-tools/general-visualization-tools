12f24d452299a1f8463c7b58726098c750f403fd
annin401 <annin401@gmail.com>
2021-10-24 15:49:23 +0900

    fix typo

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackplugin } = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -44,7 +44,7 @@ module.exports = {
     extensions: ["*", ".js", ".ts", ".tsx"],
   },
   plugins: [
-    new CleanWebpackplugin(),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "src/index.html",
     }),
