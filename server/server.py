bae094c98e6a7f35264d7a01ce55810e2efd1834
annin401 <annin401@gmail.com>
2021-11-15 14:41:14 +0900

    make template

--- server/server.py
+++ server/server.py
@@ -1,9 +1,33 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "Hello World!"
+@app.route('/projects', methods=['POST', 'GET'])
+def post_projects():
+    return "new a project"
+
+@app.route('/projects', methods=['GET'])
+def get_projects():
+    return "GET all projects info"
+
+@app.route('/projects/<projectID>', methods=['GET'])
+def get_unique_project(projectID):
+    return "get an unique projects info"
+
+@app.route('/projects/<projectID>', methods=['DELETE'])
+def delete_unique_project(projectID):
+    return "delete a project"
+
+@app.route('/projects/<projectID>/submissions', methods=['POST'])
+def post_submissions():
+    return "submit"
+
+@app.route('/projects/<projectID>/submissions', methods=['GET'])
+def get_submissions():
+    return "get all submissions"
+
+@app.route('/projects/<projectID>/submissions/<submissionID>', methods=['GET'])
+def get_unique_submission(submissionID):
+    return "get ann unique projects"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0")
